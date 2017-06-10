bad628b090d89608d5937fac42e26faf94359067
Rajesh Bamroteeya <rajbamrotiya@users.noreply.github.com>
2017-06-10 11:16:23 +0530

    add ide helper file
    add ide helper file

---
+++ my_models.php
@@ -0,0 +1,26 @@
+<?php die();
+
+/**
+ * Add you custom models here that you are loading in your controllers
+ *
+ * <code>
+ * $this->site_model->get_records()
+ * </code>
+ * Where site_model is the model Class
+ *
+ * ---------------------- Models to Load ----------------------
+ * <examples>
+ *
+ * @property user_model       $user_model
+ * @property common_model     $common_model
+ * @property setting_model    $setting_model
+ * @property admin_model      $admin_model
+ *
+ *
+ *
+ */
+class my_models
+{
+}
+
+// End my_models.php
